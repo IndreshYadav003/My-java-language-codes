09cda4985fb667001ed182dd63f6739c947456c2
Indresh Yadav <147027546+IndreshYadav003@users.noreply.github.com>
2024-05-27 07:05:46 +0530

    method overloading with object

---
+++ addmethod.java
@@ -0,0 +1,26 @@
+public class addmethod {
+    public   addmethod(){
+        System.out.println("Sum of numbrs");
+    }
+    //add number both integer
+    public static int add(int a,int b){
+        return  a+b;
+    }
+    //add number both float
+    public static float add(float a,float b){
+        return  a+b;
+    }
+    //add number 1 int and 2 double
+    public static double add(double a,int b){
+        return  a+b;
+    }
+    public static double add(int a,double b){
+        return  a+b;
+    }
+    public static int add(int a,int b,int c){
+        return  a+b+c;
+    }
+    public static float add(float a,float b,float c){
+        return a+b+c;
+    }
+}
---
+++ addtion.java
@@ -0,0 +1,7 @@
+public class addtion {
+    public static void main(String[] args) {
+        addmethod a=new addmethod();
+        System.out.println(a.add(4,2));
+        System.out.println(a.add(3.9,5));
+    }
+}
