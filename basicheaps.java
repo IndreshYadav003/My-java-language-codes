f720fca4134a855dd6d853324acf1c3744abb1a7
Indresh Yadav <147027546+IndreshYadav003@users.noreply.github.com>
2024-06-17 05:08:57 +0530

    heaps basic operation

---
+++ basicheaps.java
@@ -0,0 +1,23 @@
+package heaps;
+import java.util.*;
+public class basicheaps {
+    public static void main(String[] args) {
+        PriorityQueue<Integer> hp = new PriorityQueue<>();//min heap
+        hp.add(42);
+        System.out.println(hp);
+        hp.add(99);
+        System.out.println(hp);
+        hp.add(33);
+        hp.add(3);
+        hp.add(1);
+        System.out.println(hp);
+        System.out.print("size of heap"+hp.size());
+        System.out.println("peek element of heap"+hp.peek());
+
+        hp.remove();
+        hp.remove();
+        System.out.println(hp);
+        System.out.print("size of heap"+hp.size());
+        System.out.println("peek element of heap"+hp.peek());
+    }
+}
